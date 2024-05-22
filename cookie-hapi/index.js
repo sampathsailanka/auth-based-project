bab12f6881c6e51bd146674d3ab493b6c3a687f4
sampathsailanka <sampathsm144@gmail.com>
2024-05-22 10:42:04 +0530

    updating index.js file and changed the migration format from version 1 to 2

--- cookie-hapi/index.js
+++ cookie-hapi/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const HapiCookie = require("@hapi/cookie");
+const DBMigrate = require("db-migrate");
+const { Pool } = require("pg");
+const CRDB = require("crdb-pg");
 
 const {
   loginRoute,
@@ -11,74 +14,98 @@ const {
   registerRoute,
 } = require("./handler");
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
-  });
+const dbm = DBMigrate.getInstance(true, { throwUncatched: true });
 
-  await server.register(HapiCookie);
+const server = Hapi.server({
+  port: 3000,
+  host: "localhost",
+});
 
-  server.auth.strategy("session", "cookie", {
-    cookie: {
-      name: "user",
-      password: process.env.IRON_PASSWORD,
-      isSecure: false,
-      isHttpOnly: true,
-      path: "/",
-      ttl: 15 * 60 * 1000,
-    },
-    validate: validateFunc,
-  });
+async function start() {
+  try {
+    await server.start();
+    console.log("server running on %s", server.info.uri);
+  } catch (err) {
+    console.error(`Error while starting the server: ${err.message}`);
+  }
+}
 
-  server.auth.default("session");
+async function register() {
+  try {
+    await server.register(HapiCookie);
 
-  server.route([
-    {
-      method: "GET",
-      path: "/",
-      handler: (req, h) => {
-        return "Hello World!!! from hapi";
+    server.auth.strategy("session", "cookie", {
+      cookie: {
+        name: "user",
+        password: process.env.IRON_PASSWORD,
+        isSecure: false,
+        isHttpOnly: true,
+        path: "/",
+        ttl: 15 * 60 * 1000,
       },
-      options: {
-        auth: false,
-      },
-    },
-    {
-      method: "GET",
-      path: "/main",
-      handler: mainRoute,
-    },
-    {
-      method: "POST",
-      path: "/register",
-      handler: registerRoute,
-      options: {
-        auth: false,
-      },
-    },
-    {
-      method: "POST",
-      path: "/login",
-      handler: loginRoute,
-      options: {
-        auth: false,
-      },
-    },
-    {
-      method: "GET",
-      path: "/logout",
-      handler: logoutRoute,
-    },
-  ]);
+      validate: validateFunc,
+    });
 
-  await server.start();
-  console.log("server running on %s", server.info.uri);
+    server.auth.default("session");
 
-  process.on("unhandledRejection", (err) => {
-    console.log(err);
-    process.exit(1);
-  });
-};
+    await dbm.up();
 
-init();
+    const dbConfig = dbm.config.getCurrent().settings;
+
+    // const _tempPGPlugin = {
+    //   register: async (server, options) => {
+    //     const pool = new Pool(options.config);
+    //     server.expose("pool", pool);
+    //   },
+    //   name: "pg",
+    // };
+
+    const _tempPGPlugin = {
+      register: (request, options) => {
+        const crdb = new CRDB(options.config);
+        const pool = crdb.pool();
+        request.expose("pool", pool);
+      },
+      name: "pg",
+    };
+
+    await server.register({
+      plugin: _tempPGPlugin,
+      options: { config: dbConfig, connectionCount: 8 },
+    });
+
+    server.route([
+      {
+        method: "GET",
+        path: "/",
+        handler: (req, h) => "Hello World!!! from hapi",
+        options: { auth: false },
+      },
+      { method: "GET", path: "/main", handler: mainRoute },
+      {
+        method: "POST",
+        path: "/register",
+        handler: registerRoute,
+        options: { auth: false },
+      },
+      {
+        method: "POST",
+        path: "/login",
+        handler: loginRoute,
+        options: { auth: false },
+      },
+      { method: "GET", path: "/logout", handler: logoutRoute },
+    ]);
+
+    return true;
+  } catch (err) {
+    console.error(`Error during registration: ${err.message}`);
+    return false;
+  }
+}
+
+(async () => {
+  if (await register()) {
+    start();
+  }
+})();
--- cookie-hapi/migrations/20240521050328-cookiestore.js
+++ cookie-hapi/migrations/20240521050328-cookiestore.js
@@ -1,21 +1,8 @@
 "use strict";
 
-var dbm;
-var type;
-var seed;
-
-/**
- * We receive the dbmigrate dependency from dbmigrate initially.
- * This enables us to not have to rely on NODE_PATH.
- */
-exports.setup = function (options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-};
-
-exports.up = function (db) {
-  return db.createTable("users", {
+exports.migrate = async (db, opt) => {
+  const type = opt.dbm.dataType;
+  await db.createTable("users", {
     id: {
       type: "uuid",
       primaryKey: true,
@@ -33,10 +20,6 @@ exports.up = function (db) {
   });
 };
 
-exports.down = function (db) {
-  return db.dropTable("users", { ifExists: true });
-};
-
 exports._meta = {
-  version: 1,
+  version: 2,
 };
